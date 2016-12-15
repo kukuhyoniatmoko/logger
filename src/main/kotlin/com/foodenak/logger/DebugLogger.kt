0e4261b23c1fc401ef83278691c56decd973efbc
Kukuh Yoniatmoko <kukuhyoniatmoko@gmail.com>
2016-12-15 12:41:08 +0700

    add test for DebugLogger

--- src/main/kotlin/com/foodenak/logger/DebugLogger.kt
+++ src/main/kotlin/com/foodenak/logger/DebugLogger.kt
@@ -2,26 +2,24 @@ package com.foodenak.logger
 
 import java.util.regex.Pattern
 
+/**
+ * [Logger] for debugging, synthetically extract caller class name as tag if tag is null
+ * Do not use [this][DebugLogger] with proguard enabled
+ */
 abstract class DebugLogger() : Logger() {
-    override final fun log(priority: Priority, tag: String?, message: String) {
-        val preparedTag: String
-        if (tag == null) {
-            val trace = Throwable().stackTrace
-            if (trace.size <= CALL_STACK_INDEX) throw IllegalStateException("Using proguard?")
-            val className = trace[CALL_STACK_INDEX].className
-            val matcher = ANONYMOUS_CLASS.matcher(className)
-            val name = if (matcher.find()) matcher.replaceAll("") else className
-            preparedTag = name.substring(name.lastIndexOf('.') + 1)
-        } else {
-            preparedTag = tag
-        }
-        debugLog(priority, preparedTag, message)
+    override public final fun getTag(): String {
+        val tag = super.getTag()
+        if (tag != null) return tag
+        val trace = Throwable().stackTrace
+        if (trace.size <= CALL_STACK_INDEX) throw IllegalStateException("Using proguard?")
+        val className = trace[CALL_STACK_INDEX].className
+        val matcher = ANONYMOUS_CLASS.matcher(className)
+        val name = if (matcher.find()) matcher.replaceAll("") else className
+        return name.substring(name.lastIndexOf('.') + 1)
     }
-
-    abstract fun debugLog(priority: Priority, preparedTag: String, message: String)
 
     companion object {
         private val ANONYMOUS_CLASS = Pattern.compile("(\\$\\d+)+$")
-        private const val CALL_STACK_INDEX = 5
+        private const val CALL_STACK_INDEX = 7
     }
 }
--- src/main/kotlin/com/foodenak/logger/Log.kt
+++ src/main/kotlin/com/foodenak/logger/Log.kt
@@ -3,95 +3,121 @@ package com.foodenak.logger
 import java.util.*
 
 object Log {
-    @JvmStatic fun v(throwable: Throwable? = null, tag: String? = null, message: String? = null, args: Array<String> = arrayOf()) {
-        LOGGERS.v(throwable, tag, message, args)
+
+    /** Log with [Priority.VERBOSE] */
+    @JvmStatic fun v(throwable: Throwable? = null, message: String? = null, args: Array<out Any?> = arrayOf()) {
+        engine.v(throwable, message, args)
     }
 
-    @JvmStatic fun d(throwable: Throwable? = null, tag: String? = null, message: String? = null, args: Array<String> = arrayOf()) {
-        LOGGERS.d(throwable, tag, message, args)
+    /**
+     * Log with [Priority.DEBUG]
+     */
+    @JvmStatic fun d(throwable: Throwable? = null, message: String? = null, args: Array<out Any?> = arrayOf()) {
+        engine.d(throwable, message, args)
     }
 
-    @JvmStatic fun i(throwable: Throwable? = null, tag: String? = null, message: String? = null, args: Array<String> = arrayOf()) {
-        LOGGERS.i(throwable, tag, message, args)
+    /**
+     * Log with [Priority.INFO]
+     */
+    @JvmStatic fun i(throwable: Throwable? = null, message: String? = null, args: Array<out Any?> = arrayOf()) {
+        engine.i(throwable, message, args)
     }
 
-    @JvmStatic fun w(throwable: Throwable? = null, tag: String? = null, message: String? = null, args: Array<String> = arrayOf()) {
-        LOGGERS.w(throwable, tag, message, args)
+    /**
+     * Log with [Priority.WARNING]
+     */
+    @JvmStatic fun w(throwable: Throwable? = null, message: String? = null, args: Array<out Any?> = arrayOf()) {
+        engine.w(throwable, message, args)
     }
 
-    @JvmStatic fun e(throwable: Throwable? = null, tag: String? = null, message: String? = null, args: Array<String> = arrayOf()) {
-        LOGGERS.e(throwable, tag, message, args)
+    /**
+     * Log with [Priority.ERROR]
+     */
+    @JvmStatic fun e(throwable: Throwable? = null, message: String? = null, args: Array<out Any?> = arrayOf()) {
+        engine.e(throwable, message, args)
     }
 
-    @JvmStatic fun wtf(throwable: Throwable? = null, tag: String? = null, message: String? = null, args: Array<String> = arrayOf()) {
-        LOGGERS.wtf(throwable, tag, message, args)
+    /**
+     * Log with [Priority.ASSERT]
+     */
+    @JvmStatic fun wtf(throwable: Throwable? = null, message: String? = null, args: Array<out Any?> = arrayOf()) {
+        engine.wtf(throwable, message, args)
     }
 
-    @JvmStatic fun log(priority: Priority, throwable: Throwable? = null, tag: String? = null, message: String? = null, args: Array<String> = arrayOf()) {
-        LOGGERS.log(priority, throwable, tag, message, args)
+    /**
+     * Log with [priority]
+     */
+    @JvmStatic fun log(priority: Priority, throwable: Throwable? = null, message: String?, args: Array<out Any?> = arrayOf()) {
+        engine.log(priority, throwable, message, args)
     }
 
-    private val EMPTY_LOGGER_ARRAY = arrayOf<Logger>()
+    /** Set tag to be used on the next logging */
+    @JvmStatic fun tag(tag: String): Logger {
+        val loggers = loggerArray
+        for (logger in loggers) logger.explicitTag.set(tag)
+        return engine
+    }
 
-    private val LOGGER_LIST = HashSet<Logger>()
-    private var LOGGER_ARRAY = EMPTY_LOGGER_ARRAY
+    internal val emptyLoggerArray = arrayOf<Logger>()
+    internal val loggerList = ArrayList<Logger>()
+    internal var loggerArray = emptyLoggerArray
 
     @JvmStatic fun add(logger: Logger) {
-        if (logger == LOGGERS) throw IllegalArgumentException()
-        synchronized(LOGGER_LIST) {
-            LOGGER_LIST.add(logger)
-            LOGGER_ARRAY = LOGGER_LIST.toTypedArray()
+        if (logger == engine) throw IllegalArgumentException("Can't add engine into itself.")
+        synchronized(loggerList) {
+            loggerList.add(logger)
+            loggerArray = loggerList.toTypedArray()
         }
     }
 
     @JvmStatic fun remove(logger: Logger) {
-        synchronized(LOGGER_LIST) {
-            if (!LOGGER_LIST.remove(logger)) throw IllegalStateException()
-            LOGGER_ARRAY = if (LOGGER_LIST.isEmpty()) EMPTY_LOGGER_ARRAY else LOGGER_LIST.toTypedArray()
+        synchronized(loggerList) {
+            if (!loggerList.remove(logger)) throw IllegalStateException("Logger is not added yet.")
+            loggerArray = if (loggerList.isEmpty()) emptyLoggerArray else loggerList.toTypedArray()
         }
     }
 
-    @JvmStatic fun asLogger(): Logger = LOGGERS
+    @JvmStatic fun asLogger(): Logger = engine
 
-    @JvmStatic fun loggers(): List<Logger> = listOf(*LOGGER_ARRAY)
+    @JvmStatic fun loggers(): List<Logger> = listOf(*loggerArray)
 
-    private val LOGGERS = object : Logger() {
-        override fun v(throwable: Throwable?, tag: String?, message: String?, args: Array<String>) {
-            val loggers = LOGGER_ARRAY
-            for (logger in loggers) logger.v(throwable, tag, message, args)
+    @JvmStatic private val engine = object : Logger() {
+        override fun v(throwable: Throwable?, message: String?, args: Array<out Any?>) {
+            val loggers = loggerArray
+            for (logger in loggers) logger.v(throwable, message, args)
         }
 
-        override fun d(throwable: Throwable?, tag: String?, message: String?, args: Array<String>) {
-            val loggers = LOGGER_ARRAY
-            for (logger in loggers) logger.d(throwable, tag, message, args)
+        override fun d(throwable: Throwable?, message: String?, args: Array<out Any?>) {
+            val loggers = loggerArray
+            for (logger in loggers) logger.d(throwable, message, args)
         }
 
-        override fun i(throwable: Throwable?, tag: String?, message: String?, args: Array<String>) {
-            val loggers = LOGGER_ARRAY
-            for (logger in loggers) logger.i(throwable, tag, message, args)
+        override fun i(throwable: Throwable?, message: String?, args: Array<out Any?>) {
+            val loggers = loggerArray
+            for (logger in loggers) logger.i(throwable, message, args)
         }
 
-        override fun w(throwable: Throwable?, tag: String?, message: String?, args: Array<String>) {
-            val loggers = LOGGER_ARRAY
-            for (logger in loggers) logger.w(throwable, tag, message, args)
+        override fun w(throwable: Throwable?, message: String?, args: Array<out Any?>) {
+            val loggers = loggerArray
+            for (logger in loggers) logger.w(throwable, message, args)
         }
 
-        override fun e(throwable: Throwable?, tag: String?, message: String?, args: Array<String>) {
-            val loggers = LOGGER_ARRAY
-            for (logger in loggers) logger.e(throwable, tag, message, args)
+        override fun e(throwable: Throwable?, message: String?, args: Array<out Any?>) {
+            val loggers = loggerArray
+            for (logger in loggers) logger.e(throwable, message, args)
         }
 
-        override fun wtf(throwable: Throwable?, tag: String?, message: String?, args: Array<String>) {
-            val loggers = LOGGER_ARRAY
-            for (logger in loggers) logger.wtf(throwable, tag, message, args)
+        override fun wtf(throwable: Throwable?, message: String?, args: Array<out Any?>) {
+            val loggers = loggerArray
+            for (logger in loggers) logger.wtf(throwable, message, args)
         }
 
-        override fun log(priority: Priority, throwable: Throwable?, tag: String?, message: String?, args: Array<String>) {
-            val loggers = LOGGER_ARRAY
-            for (logger in loggers) logger.log(priority, throwable, tag, message, args)
+        override fun log(priority: Priority, throwable: Throwable?, message: String?, args: Array<out Any?>) {
+            val loggers = loggerArray
+            for (logger in loggers) logger.log(priority, throwable, message, args)
         }
 
-        override fun log(priority: Priority, tag: String?, message: String) {
+        override fun println(priority: Priority, tag: String?, message: String) {
             throw AssertionError("Missing override!")
         }
     }
